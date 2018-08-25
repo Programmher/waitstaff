42e1094618c41360fd9dcb8ebc1863e4b912f8b3
Randall Randall <randall@randallsquared.com>
2018-08-25 16:44:29 -0400

    break out functionality into testable units

--- index.js
+++ index.js
@@ -1,96 +1,18 @@
 #!/usr/bin/env node 
 
-const { Socket } = require('net');
-const { exit, argv, cwd, env } = require('process');
-const { execSync } = require('child_process');
-const [,, ...params] = argv;
-const USAGE = `
-Usage:
-  $cmdname [host]:port [-q] [-t <timeout>] [-- command args]
-  -q | --quiet                        Do not output any status messages
-  -t <timeout> | --timeout=<timeout>  Timeout in seconds, zero for no timeout
-  -- COMMAND ARGS                     Execute command with args after the test finishes
-`;
+const { exit } = require('process');
+const { complain, after } = require('./lib/utils');
+const waiter = require('./lib/waiter');
+const Opts = require('./lib/options');
+const opts = new Opts();
+const { host, port, command, quiet, timeout } = opts.parseOrQuit();
 
-const timeoutLongOpt = '--timeout';
-let port;
-let command;
-let host = 'localhost';
-let ms = 200;
-let quiets = ['-q', '--quiet'];
-let timeouts = ['-t', timeoutLongOpt];
-let note = (msg, type='log') => console[type](msg);
-const timeoutAt = (millis) => { [timeouts, ms] = [[], parseInt(millis, 10)]; };
-const after = ms => new Promise(resolve => setTimeout(resolve, ms));
-const complain = (msg) => {
-  note(msg, 'error');
-  exit(1);
-};
-const usage = () => {
-  complain(USAGE);
-};
-
-while(params.length) {
-    const param = params.shift();
-    if(quiets.includes(param)) {
-      note = () => false;
-      quiets = [];
-    } else if(timeouts.includes(param)) {
-      // next param should therefore be a value in millis
-      if(params.length && params[0].match(/\d+/)) {
-        timeoutAt(params.shift());
-      } else {
-        usage();
-      }
-    } else if(param.startsWith(`${timeoutLongOpt}=`)) {
-      timeoutAt(param.replace(`${timeoutLongOpt}=`, ''));
-    } else if(param==='--') {
-      // everything after this is a command to execute when we're done
-      if(!params.length) usage();
-      command = params.join(' ');
-      break;
-    } else {
-      [host, port] = param.split(':');
-      port = parseInt(port, 10);
-      if(host && port) continue;
-      usage();
-    }
-}
-
-// By now we should definitely have a port.
-// Every other parameter has sane defaults.
-if(!port) usage();
-
-const main = async (host, port, command) => {
-
-  const canConnect = async () => {
-    return new Promise((resolve, reject) => {
-      const connection = (new Socket()).connect(port, host);
-      connection.on('connect', () => resolve(true));
-      connection.on('error', reject);
-    });
-  };
-
-  while(true) {
-    try {
-      if(await canConnect()) break;
-    } catch (e) {}
-    await after(50);
-  }
-
-  if(command) {
-    note(`Running \`${command}\``);
-    execSync(command, { cwd: cwd(), stdio: 'inherit', env });
-  }
-};
-
-main(host, port, command)
+waiter(host, port, command, quiet)
   .then(() => exit(0))
   .catch((e) => {
-      if(command && e.status) exit(e.status);
-      else complain('unknown error');
-    });
+    if(command && e.status) exit(e.status);
+    else complain(`unknown error: ${e.message}`);
+  });
 
-note(`waiting for ${host}:${port} ${ms ? `for ${ms} ms.` : 'forever.'}`);
-if(ms) after(ms).then(() => complain('timed out; no connection available'));
-
+if(!quiet) console.log(`waiting for ${host}:${port} ${timeout ? `for ${timeout} ms.` : 'forever.'}`);
+if(timeout) after(timeout).then(() => complain('timed out; no connection available'));
